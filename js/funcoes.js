23a0bfabd3283b14d0017ed0dc5f5d735f3f34bc
Joao Pedro <jpremedi@hotmail.com>
2020-12-07 22:18:43 -0300

    exercicio 1

---
+++ js/funcoes.js
@@ -0,0 +1,31 @@
+console.log("Segue o líder")
+
+let nome = prompt("Informe seu nome");
+alert("E aí, tricolor " + nome +  ". Seja bem vindo! E segue o líder...");
+alert("O Palmeiras não tem MUNDIAL.");
+alert("#ChoraVARmengo");
+
+
+/* Boas práticas para nome de variáveis
+ - não utilizar caracteres especiais
+ - não começar com números
+ - não utilizar espaços
+ - ter nome significativo
+ - manter nome em minúsculo
+ - quando for palavra composta, colocar as inciais a partir da segunda palavra em maiúsculo
+ - o js diferencia minúsculas e MAIÚSCULAS (case sensitive) 
+ - o js é fracamente tipado
+ texto "Jake"
+ numérico 10
+    inteiro 10
+    decimal 10.5
+ lógico/booleano --> true / false --> 1 / 0
+    10 > 7 -> true
+    tipos de dados básicos do javascript são:
+    - string --> "texto"
+    - number --> 10 ou 10.5
+    - boolean --> true / false
+    = --> atribuição
+    + --> concatenação quando é com texto
+    + --> adição quando é só com números
+ */
